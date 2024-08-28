2e06cb71daa7254b8d5740d430521368453b2165
sanchez120 <jonatangremory120@gmail.com>
2024-08-28 17:45:11 -0500

    feat(back-end): se agrego dos metodos a customer.cs

---
+++ orders/BLL/Exceptions/SuppliersExceptions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Exceptions
+{
+    internal class SuppliersExceptions : Exception
+    {
+        public SuppliersExceptions() : base("No Supplier found in the database.")
+        {
+        }
+
+        private SuppliersExceptions(string message) : base(message)
+        {
+        }
+
+        public static void ThrowSupplierAlreadyExistsException(string companyName, string contactName)
+        {
+            throw new SuppliersExceptions($"A Supplier with the name '{companyName}' and contact '{contactName}' already exists.");
+        }
+
+        public static void ThrowInvalidSupplierIdException(int id)
+        {
+            throw new SuppliersExceptions($"Invalid Supplier ID: {id}");
+        }
+    }
+}
+
+
+
+   
